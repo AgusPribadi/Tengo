4053aeb9ee03b05f80a9bf5d4de78180132d787b
Agus Pribadi <aguspribadi368@gmail.com>
2023-08-12 13:40:20 +0700

    perbarui pencarian

--- aplikasi_tengo/views.py
+++ aplikasi_tengo/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import CoffeeShop
+from django.db.models import Q
 
 def index(request):
     return render(request, 'index.html')
 
 def home(request):
-    coffee_shops = CoffeeShop.objects.all()
+    query = request.GET.get('q')
+    if query:
+        coffee_shops = CoffeeShop.objects.filter(Q(nama__icontains=query) | Q(alamat__icontains=query))
+    else:
+        coffee_shops = CoffeeShop.objects.all()
+    
     return render(request, 'home.html', {'coffee_shops': coffee_shops})
 
 def detail_coffeeshop(request, coffee_shop_id):
@@ -14,3 +20,6 @@ def detail_coffeeshop(request, coffee_shop_id):
         'coffee_shop': coffee_shop,
     }
     return render(request, 'detail_coffeeshop.html', context)
+
+def about(request):
+    return render(request, 'about.html')
